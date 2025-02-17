fa975b64d3e648fcf020ee311cf297966f3ad477
1Hunor4 <c2verhun@vasvari.org>
2025-02-17 09:17:25 +0100

    napok + nem költés + átlag def

--- vasarlas.py
+++ vasarlas.py
@@ -11,3 +11,21 @@ def nap():
     print(napok, "nap volt a hónapban.")
 nap()
 
+def nemkoltes():
+    uresnap = 0
+    for i in sor:
+        if i == "0":
+            uresnap += 1
+    print(uresnap, "napon nem történt költés.")
+nemkoltes()
+
+def napiatlag():
+    szum = 0
+    db = 0
+    for i in sor:
+        szum += int(i)
+        db += 1
+    atl = szum/db
+    print("A napi átlagos költés:",round(atl),"ft")
+napiatlag()
+
